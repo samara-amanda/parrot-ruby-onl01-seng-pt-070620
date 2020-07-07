c0c746f9e885c9e754a23169885c0c8e1bc5a48d
Samara Grant <samara.amanda01@gmail.com>
2020-07-07 13:14:25 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase="Squawk!")
-  puts "Pretty bird!"
   return "Pretty bird!"
+  puts "Pretty bird!"
 end
